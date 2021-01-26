503c90213b8281ed1aba1a28b77a0dcdd8ce64cc
Richard Adule <big.richi@outlook.com>
2021-01-26 18:03:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,9 @@ def key_for_min_value(name_hash)
   min_item = ""
   min_value = 9999
   name_hash.each do |item, value|
+    if item = "" 
+      return nil 
+    end 
     if value < min_value 
       min_value = value
       min_item = item
